3b3784adc1c613d787b3e59d7844ab05780d9929
joithedogfather <joithedogfather@gmail.com>
2019-08-19 17:49:25 +0900

    Unreal Class day 1

---
+++ Project1/project1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int MyGold = 3;
+	int AddGold = 55;
+	int NewGold = MyGold + AddGold;
+
+	printf("%d Gold + %d Gold = %d Gold", MyGold, AddGold, NewGold);
+
+		return 0;
+
+}
